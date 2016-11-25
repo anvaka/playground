67d9e7cb39f0739e4427301c10200117b89cdc66
anvaka <anvaka@gmail.com>
2016-11-25 14:48:08 -0800

    Simple words counting

---
+++ collect-words/index.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const fs = require('fs');
+
+var fileName = process.argv[2] || 'martin_eden_ru.txt';
+const rl = readline.createInterface({
+  input: fs.createReadStream(fileName)
+});
+
+var ignoreWords = new Set(require('./stowords_ru.js'))
+
+var counts = new Map();
+var maxValue = 0;
+var maxFontSize = 50;
+rl.on('line', (line) => {
+  line.split(/\s/).forEach(p => {
+    p = p.toLowerCase().replace(/[-?,!."']/g, '');
+
+    if (ignoreWords.has(p) || !p) return;
+    var newCount = (counts.get(p) || 0) + 1;
+    counts.set(p, newCount)
+    if (newCount > maxValue) maxValue = newCount;
+  });
+}).on('close', () => {
+  var words = Array.from(counts).sort((x, y) => y[1] - x[1])
+  .map(x => {
+    return [x[0], Math.round(maxFontSize * x[1]/maxValue) + 3]
+  }).slice(0, 1500)
+  console.log(JSON.stringify(words));
+});
