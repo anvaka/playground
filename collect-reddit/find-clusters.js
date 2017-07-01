669da55af010909137a9bd4caa98458d65a556c2
Andriy Kashcha <anvaka@gmail.com>
2017-07-01 11:05:18 -0700

    Use smaller graph for experiments

--- collect-reddit/find-clusters.js
+++ collect-reddit/find-clusters.js
@@ -1,6 +1,5 @@
 // Let's say you have an ngraph instance:
 const createGraph = require('ngraph.graph');
-const graph = createGraph({uniqueLinkId: false});
 const downloadedFile = process.argv[3] || 'abouts.json';
 const forEachSub = require('./lib/forEachSub.js');
 const extractRelated = require('./lib/extract-related.js');
@@ -12,40 +11,58 @@ const path = require('path');
 const outFolder = path.join('data', 'clusters');
 const mkdirp = require('mkdirp');
 
+// const graph = createGraph({uniqueLinkId: false});
+
 mkdirp.sync(outFolder);
 
 // // To detect clusters:
 //
-forEachSub(downloadedFile, (sub) => {
-  const subName = sub.display_name.toLowerCase();
-  const subInfo = extractRelated(sub);
+// forEachSub(downloadedFile, (sub) => {
+//   const subName = sub.display_name.toLowerCase();
+//   const subInfo = extractRelated(sub);
+//
+//   graph.addNode(subName, sub.subscribers);
+//
+//   if (subInfo.related.length > 0) {
+//     subInfo.related.forEach(x => {
+//       graph.addLink(subName, x);
+//     });
+//   }
+// }, saveClusters);
 
-  graph.addNode(subName, sub.subscribers);
-
-  if (subInfo.related.length > 0) {
-    subInfo.related.forEach(x => {
-      graph.addLink(subName, x);
-    });
-  }
-}, saveClusters);
+const graph = require('miserables');
+saveClusters();
 
 function saveClusters() {
-  const clusters = detectClusters(graph);
+  let prevGraph = graph;
+  let clusters = detectClusters(graph);
+  let lastGraph = coarsen(graph, clusters);
+  let layer = 0;
+  while(clusters.canCoarse()) {
+    debugger;
+    saveLayer(layer++);
+    prevGraph = lastGraph;
+    lastGraph = coarsen(prevGraph, clusters);
+    clusters = detectClusters(lastGraph);
+  }
 
-  const clusterGraph = coarsen(graph, clusters);
-  clusterGraph.forEachNode(coarseNode => {
-    const nodeGraph = createGraph({uniqueLinkId: false});
+  function saveLayer(layerNumber) {
+    lastGraph.forEachNode(coarseNode => {
+      const nodeGraph = createGraph({uniqueLinkId: false});
 
-    coarseNode.data.forEach(nodeId => {
-      nodeGraph.addNode(nodeId, graph.getNode(nodeId).data);
+      coarseNode.data.forEach(nodeId => {
+        let data = prevGraph.getNode(nodeId).data
+        let nodes = (data instanceof Set) ? Array.from(data) : data
+        nodeGraph.addNode(nodeId, { nodes: nodes });
 
-      graph.forEachLinkedNode(nodeId, otherNode => {
-        if (coarseNode.data.has(otherNode.id)) {
-          nodeGraph.addLink(nodeId, otherNode.id)
-        }
-      }, true);
-    });
+        graph.forEachLinkedNode(nodeId, otherNode => {
+          if (coarseNode.data.has(otherNode.id)) {
+            nodeGraph.addLink(nodeId, otherNode.id)
+          }
+        }, true);
+      });
 
-    fs.writeFileSync(path.join(outFolder, coarseNode.id + '.json'), tojson(nodeGraph));
-  })
+      fs.writeFileSync(path.join(outFolder, layerNumber + '_' + coarseNode.id + '.json'), tojson(nodeGraph));
+    })
+  }
 }
