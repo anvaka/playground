3b81d9568fdb3554aa8dd7756dfda0a1cab800f0
Andriy Kashcha <anvaka@gmail.com>
2017-07-15 22:01:13 -0700

    Fixed a bug with point duplicates

--- manual-layout/src/lib/smoothPath.js
+++ manual-layout/src/lib/smoothPath.js
@@ -3,16 +3,19 @@ module.exports = smoothPath;
 function smoothPath(path) {
   let prevSegment = segment(path[0], path[1]);
   let svg_path = 'M' + point(path[0])
+  let splitRatio = 0.55
 
   for (var i = 1; i < path.length - 1; i++) {
-    let prevSplit = splitSegment(prevSegment, 0.75)
+    let prevSplit = splitSegment(prevSegment, splitRatio)
     svg_path += 'L' + point(prevSplit[1]) + ' '
+
+    if (isSame(path[i], path[i + 1])) continue;
 
     let nextSegment = segment(path[i], path[i + 1])
     let lastControlPointSegment = segment(prevSplit[1], prevSplit[2])
     let lastControlPointSplit = splitSegment(lastControlPointSegment, 0.5)
 
-    let nextSplit = splitSegment(nextSegment, 0.25)
+    let nextSplit = splitSegment(nextSegment, 1 - splitRatio)
     let nextControlPointSegment = segment(nextSplit[0], nextSplit[1]);
     let nextControlPointSplit = splitSegment(nextControlPointSegment, 0.5)
 
@@ -28,7 +31,14 @@ function smoothPath(path) {
   return svg_path;
 
 
+  function isSame(p0, p1) {
+    return p0.x === p1.x && p0.y === p1.y;
+  }
+
   function point(p) {
+    if (Number.isNaN(p.x) || Number.isNaN(p.y)) {
+      throw new Error('Nan')
+    }
     return p.x + ',' + p.y
   }
   function segment(from, to) {
