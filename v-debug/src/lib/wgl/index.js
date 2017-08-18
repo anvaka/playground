dd2be03d266c07a405ec6f47f702fb718eda9edf
Andriy Kashcha <anvaka@gmail.com>
2017-08-17 19:34:55 -0700

    Expose element on the wgl level
    This can serve as a groupping mechansim

--- v-debug/src/lib/wgl/index.js
+++ v-debug/src/lib/wgl/index.js
@@ -1,7 +1,12 @@
+// TODO: I need better names.
 module.exports = {
   scene: require('./scene'),
+
   Points: require('./Points'),
   Point: require('./Point'),
+  
   Lines: require('./Lines'),
   Line: require('./Line'),
+
+  Element: require('./Element'),
 }
