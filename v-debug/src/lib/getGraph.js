0387ea1aa531a28db74946da76de45384091684d
Andriy Kashcha <anvaka@gmail.com>
2017-08-26 00:54:39 -0700

    added small graph for tests

--- v-debug/src/lib/getGraph.js
+++ v-debug/src/lib/getGraph.js
@@ -1,17 +1,29 @@
 var fromJson = require('ngraph.fromjson');
 var data = require('../data/anvaka_twitter.json')
-// var createGraph = require('ngraph.graph');
+var createGraph = require('ngraph.graph');
 
 module.exports = getGraph;
+let useSmall = false;
 
 function getGraph() {
-  let graph = fromJson(data);
-  // let graph = createGraph();
-  // graph.addLink(1, 2);
-  // graph.addLink(1, 4);
-  // graph.addLink(1, 3);
-  // graph.addLink(4, 6);
-  // don't let vue modify this, it's unnecessary
-  Object.freeze(graph);
+  let graph = useSmall ? smallGraph() : fromJson(data);
+  return graph;
+}
+
+function smallGraph() {
+  let graph = createGraph();
+  graph.addLink(0, -1);
+  graph.addLink(0, 1);
+  graph.addLink(1, 2);
+  graph.addLink(1, 4);
+  graph.addLink(1, 3);
+  graph.addLink(4, 6);
+  graph.addLink(6, 7);
+  graph.addLink(6, 8);
+  graph.addLink(6, 9);
+  graph.addLink(6, 10);
+  graph.addLink(10, 11);
+  graph.addLink(10, 12);
+  graph.addLink(10, 13);
   return graph;
 }
