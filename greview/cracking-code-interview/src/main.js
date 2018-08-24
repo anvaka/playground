48e125578fa9e269100ba78ead161925fcfde1b5
Andriy Kashcha <anvaka@gmail.com>
2018-08-24 00:39:10 -0700

    Added cci

---
+++ greview/cracking-code-interview/src/main.js
@@ -0,0 +1,10 @@
+import greview from 'greview-shell';
+
+const appConfig = {
+  title: 'Cracking the Coding Interview',
+  svgPath: 'map.svg',
+  detailsPath: 'details.html',
+  graphPath: 'graph.json'
+};
+
+greview(appConfig);
