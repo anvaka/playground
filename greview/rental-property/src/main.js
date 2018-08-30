16b2b39b3f7fa6980b930bfa0bebe6ad3d3325b3
Andriy Kashcha <anvaka@gmail.com>
2018-08-29 19:33:53 -0700

    Added rental property book

---
+++ greview/rental-property/src/main.js
@@ -0,0 +1,10 @@
+import greview from 'greview-shell';
+
+const appConfig = {
+  title: 'The Book on Rental Property Investing',
+  svgPath: 'map.svg',
+  detailsPath: 'details.html',
+  graphPath: 'graph.json'
+};
+
+greview(appConfig);
