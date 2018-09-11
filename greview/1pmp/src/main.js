5a756ead0edde092dcd36a32c116ed1b7c655dc4
Andriy Kashcha <anvaka@gmail.com>
2018-09-10 23:15:39 -0700

    added 1pmp

---
+++ greview/1pmp/src/main.js
@@ -0,0 +1,10 @@
+import greview from 'greview-shell';
+
+const appConfig = {
+  title: 'The 1-Page Marketing Plan',
+  svgPath: 'map.svg',
+  detailsPath: 'details.html',
+  graphPath: 'graph.json'
+};
+
+greview(appConfig);
