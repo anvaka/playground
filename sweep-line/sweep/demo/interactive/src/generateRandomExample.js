e5566bbae86db96c0e20cb75e3cfed4ccfce4002
Andriy Kashcha <anvaka@gmail.com>
2018-10-01 21:37:16 -0700

    support functions

--- sweep-line/sweep/demo/interactive/src/generateRandomExample.js
+++ sweep-line/sweep/demo/interactive/src/generateRandomExample.js
@@ -1,37 +1,45 @@
+var createRandom = require('ngraph.random');
+var prng = createRandom(42);
+
 var params = [
-  {
-    name: 'random',
-    args: [
-      {min: 100, max: 500},  // number of lines
-      {min: 100, max: 200},  // visible area
-    ]
-  },
-  {
-    name: 'complete',
-    args: [
-      {min: 10, max: 40},  // number of nodes
-      {min: 400, max: 600},  // visible area
-    ]
-  },
-  {
-    name: 'cube',
-    args: [
-      {min: 100, max: 150},  // number of rects
-      {min: 3, max: 10},  // variance
-    ]
-  },
-  {
-    name: 'drunkgrid',
-    args: [
-      {min: 10, max: 150},  // Row x Col
-      {min: 0.1, max: 10},  // variance
-    ]
-  },
+  // {
+  //   name: 'random',
+  //   args: [
+  //     {min: 100, max: 500},  // number of lines
+  //     {min: 100, max: 200},  // visible area
+  //   ]
+  // },
+  // {
+  //   name: 'complete',
+  //   args: [
+  //     {min: 10, max: 40},  // number of nodes
+  //     function p1(qs) {
+  //       return Math.round((10 + prng.gaussian()) * qs.p0);
+  //       // {min: 400, max: 600},  // visible area
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'cube',
+  //   args: [
+  //     {min: 100, max: 150},  // number of rects
+  //     {min: 3, max: 10},  // variance
+  //   ]
+  // },
+  // {
+  //   name: 'drunkgrid',
+  //   args: [
+  //     {min: 10, max: 150},  // Row x Col
+  //     {min: 0.1, max: 10},  // variance
+  //   ]
+  // },
   {
     name: 'triangle',
     args: [
       {min: 100, max: 500},  // Count  
-      {min: 10, max: 20},  // variance
+      function p2(qs) {
+        return Math.round(Math.sqrt(qs.p0));
+      }
     ]
   }
 ]
@@ -44,7 +52,12 @@ export default function generateRandomExample() {
     generator: generator.name
   }
   generator.args.forEach((range, idx) => {
-    qs[`p${idx}`] = Math.round(Math.random() * (range.max - range.min) + range.min);
+    var keyName = `p${idx}`;
+    if (typeof range === 'function') {
+      qs[keyName] = range(qs);
+    } else  {
+      qs[keyName] = Math.round(Math.random() * (range.max - range.min) + range.min);
+    }
   });
 
   return qs;
