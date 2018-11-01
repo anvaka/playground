c2fc772c6d5fa05a8b527168b36f5195de108c19
pnaka <anvaka@gmail.com>
2018-10-31 21:04:59 -0700

    Updated code with comments

--- reddit-sim/index.js
+++ reddit-sim/index.js
@@ -1,127 +1,219 @@
+/**
+ * This file processes redditor's comments files and produces a list of all
+ * subreddits startin with a `startFrom` letter along with all similar subreddits
+ * (similar subreddits can start on any letter).
+ *
+ * The reason for such "strange" design is becuase I cannot fit the entire
+ * index in RAM. However I can easily fit index for all subreddits that start
+ * on a given letter. This allows to process similarities in parallel batches too
+ */
+
+// This file should be produced by 0.collect_data.sh script. It lists
+// all users grouped by (user_id, subreddit_name), along with number of posts
+// into each subreddit.
+// Important assumption is that users are sorted in this file. This allows
+// me to count similarity without a need to keep track where each user posted.
+// (I.e. as long as user name the same, all subreddits that I see are "similar",
+// degree of similarity is determined once we process the entire file)
+var fileName = 'reddit_aug_2018'
+
+// maps pair of subreddits into `Counter` object. I use object and not a Map
+// because Map in my node crashes with out of memory sooner ¯\_(ツ)_/¯
+var keyToCount = {};
+
+// Gives number of unique commenters for each subreddit.
+var commentersCount = new Map();
+
+// Once all file is processed we store here list 
+var indexedSimilarity = new Map();
+var startFrom = process.argv[2] || 'a';
+var path = require('path');
+var fs = require('fs');
+var JSONStream = require('JSONStream');
+
+var outStream = createOutStream(path.join('data', 'related-' + startFrom.toLowerCase() + '.json'));
+
 let forEachLine = require('for-each-line');
-var fileName = process.argv[2];
-if (!fileName) {
-  throw new Error('File is required');
-}
+var lineCount = 0;
 
-class User {
-  constructor() {
-    this.subs = new Map();
-    this.total = 0;
+class Counter {
+  constructor(subA, subB) {
+    this.subA = subA;
+    this.subB = subB;
+    this.sim = 0;
+    this.count = 0;
   }
 
-  postedTo(sub) {
-    this.total += 1;
-    this.subs.set(sub, (this.subs.get(sub) || 0) + 1);
-  }
-
-  normalize() {
-    this.subs.forEach((score, sub) => {
-      this.subs.set(sub, score/this.total);
-    });
+  increase(instanceSimilarity) {
+    this.sim += instanceSimilarity;
+    this.count += 1;
   }
 }
+var lastUser = null;
+var lastUserSubs;
+var writeOutputFor = new Set();
 
-var usersWhoPostedToSubreddit = new Map(); // maps subreddit name to user object
-var users = new Map();
-var normalizedScores = new Map();
+function shouldBeIndexed(sub) {
+  return sub[0] === startFrom;
+}
 
 forEachLine(fileName, (line) => {
   if (!line) return;
 
+  lineCount += 1;
+  if (lineCount % 500000 === 0) console.log('Indexed lines: ', lineCount, line);
   var parts = line.split(',')
   var user = parts[0];
   var sub = parts[1];
-  if (!user || !sub) {
+  var count = Number.parseInt(parts[2], 10);
+  if (!user) {
     throw new Error('Something is wrong with this line: ' + line);
   }
-
-  var userObj = users.get(user);
-
-  if (!userObj) {
-    userObj = new User();
-    users.set(user, userObj);
+  if (!Number.isFinite(count)) {
+    console.warn('skipping ' + line + ' - invalid count');
+    return;
   }
-  userObj.postedTo(sub);
-  var subUsers = usersWhoPostedToSubreddit.get(sub);
-  if (!subUsers) {
-    subUsers = new Set();
-    usersWhoPostedToSubreddit.set(sub, subUsers);
+
+  commentersCount.set(sub, (commentersCount.get(sub) || 0) + 1);
+
+  if (lastUser !== user) {
+    recordLastUser(lastUserSubs);
+    lastUser = user;
+    lastUserSubs = [];
   }
-  subUsers.add(userObj);
+  lastUserSubs.push({sub, count});
 }).then(() => {
-  normalize();
-
-  var scores = new Map();
-  var srcSub = 'badmathematics'
-  forEachUserWhoPostedTo(srcSub, user => {
-    // var currentSubScore = user.subs.get(srcSub);
-    // currentSubScore *=  usersWhoPostedToSubreddit.get(srcSub).size;
-
-    forEachSubredditByUser(user, (subScore, subName) => {
-      var score = scores.get(subName) || 0;
-      score += 1; // subScore * currentSubScore * usersWhoPostedToSubreddit.get(subName).size;
-      scores.set(subName, score);
-    });
+  console.log('all indexed');
+  Object.keys(keyToCount).forEach(indexSimilarity);
+}).then(() => {
+  writeOutputFor.forEach(subreddit => {
+    var sims = getSimilarTo(subreddit);
+    if (sims.similar.length > 0) outStream.write(sims);
   });
-  // var srcSubScore = getSubNormalScore(srcSub);
-  var srcSubScore = usersWhoPostedToSubreddit.get(srcSub).size;
+})
 
-  // normalizeScores(scores, srcSubScore);
-  jaccardSimilarity(scores, srcSubScore);
+/**
+ * This method would take top 100 similar subreddits, and then it will
+ * cut subreddit with similarity score below `mean + standard_deviation`.
+ * Usually this gives nice bounds for the threshold.
+ */
+function getSimilarTo(subName) {
+  var similar = indexedSimilarity.get(subName);
+  similar.sort((a, b) => b.score - a.score)
+  similar = similar.slice(0, 100);
 
-  console.log(JSON.stringify(Array.from(scores).sort((x, y) => y[1] - x[1]).slice(0, 100)));
-  console.log('Done!')
-});
+  var mean = 0;
+  similar.forEach(x => mean += x.score);
+  mean /= similar.length;
 
-function normalize() {
-  users.forEach(user => {
-    user.normalize();
-  });
-}
+  var stdDev = 0;
+  similar.forEach(x => stdDev += (x.score - mean) * (x.score - mean));
+  stdDev /= similar.length;
+  stdDev = Math.sqrt(stdDev);
 
-function jaccardSimilarity(scores, postedToA) {
-  scores.forEach((sharedPosts, subName) => {
-    var postedToB = usersWhoPostedToSubreddit.get(subName).size;
-    scores.set(subName, sharedPosts/(postedToA + postedToB - sharedPosts));
-  });
-}
+  var medianIndex = Math.floor(similar.length/2);
+  var median = similar[medianIndex].score;
 
-function normalizeScores(scores, otherScore) {
-  // TODO: This probably needs to be multiplied by src subreddit score
-  scores.forEach((score, subName) => {
-    scores.set(subName, score/(otherScore * getSubNormalScore(subName)));
-  });
-}
+  var foundMatches = []
+  for (var i = 0; i < similar.length; ++i) {
+    var sim = similar[i];
+    if (sim.score - median > stdDev) {
+      foundMatches.push({
+        sub: sim.sub,
+        score: sim.score
+      })
+    } else {
+      // since array is sorted, there is nothing interesting left at this point
+      break;
+    }
+  }
 
-function getSubNormalScore(subName) {
-  var normalizedScore = normalizedScores.get(subName);
-
-  if (normalizedScore) return normalizedScore;
-
-  normalizedScore = 0;
-  forEachUserWhoPostedTo(subName, user => {
-    var currentSubScore = user.subs.get(subName);
-    normalizedScore += currentSubScore * currentSubScore;
-  });
-
-  normalizedScore = Math.sqrt(normalizedScore);
-  normalizedScores.set(subName, normalizedScore);
-  return normalizedScore;
-}
-
-function forEachUserWhoPostedTo(subName, callback) {
-  var users = usersWhoPostedToSubreddit.get(subName);
-
-  if (users) {
-    users.forEach(callback);
+  return {
+    name: subName,
+    similar: foundMatches
   }
 }
 
-function forEachSubredditByUser(user, callback) {
-  var subs = user.subs;
-  if (subs) {
-    subs.forEach(callback);
+function indexSimilarity(subredditPairKey) {
+  var counter = keyToCount[subredditPairKey];
+
+  // Regular Jaccard similarity:
+  var similarity = counter.count/(commentersCount.get(counter.subA) + commentersCount.get(counter.subB) - counter.count);
+
+  // Similarity is bi-directional. We store both ends of the edge into file:
+  var aSims = indexedSimilarity.get(counter.subA);
+  if (!aSims) {
+    aSims = [];
+    indexedSimilarity.set(counter.subA, aSims);
+  }
+  aSims.push({
+    sub: counter.subB,
+    score: similarity
+  });
+
+  var bSims = indexedSimilarity.get(counter.subB);
+  if (!bSims) {
+    bSims = [];
+    indexedSimilarity.set(counter.subB, bSims);
+  }
+
+  bSims.push({
+    sub: counter.subA,
+    score: similarity
+  });
+}
+
+function recordLastUser(subs) {
+  if (!subs) return;
+  if (subs.length < 2) return;
+  var total = 0;
+  for (var i = 0; i < subs.length; ++i) {
+    total += subs[i].count;
+  }
+
+  for (var i = 0; i < subs.length - 1; ++i) {
+    var subA = subs[i];
+    for (var j = i + 1; j < subs.length; ++j) {
+      var subB = subs[j];
+
+      var processThisPair = false;
+      if (shouldBeIndexed(subA.sub)) {
+        writeOutputFor.add(subA.sub);
+        processThisPair = true;
+      }
+      if (shouldBeIndexed(subB.sub)) {
+        writeOutputFor.add(subB.sub);
+        processThisPair = true;
+      }
+      if (!processThisPair) {
+        continue;
+      }
+
+      var key = makeKey(subA.sub, subB.sub);
+      let scores = keyToCount[key];
+      if (!scores) {
+        scores = new Counter(subA.sub, subB.sub);
+        keyToCount[key] = scores;
+      }
+
+      var na = subA.count/total;
+      var nb = subB.count/total;
+
+      scores.increase((na + nb)/2);
+    }
   }
 }
 
+function makeKey(subA, subB) {
+  return subA < subB ? subA + '|' + subB : subB + '|' + subA;
+}
+
+function createOutStream(outFileName) {
+  var outgoing = JSONStream.stringify(false);
+  var fileStream = fs.createWriteStream(outFileName, {
+    encoding: 'utf8',
+    flags: 'a'
+  });
+  outgoing.pipe(fileStream);
+  return outgoing;
+}
