bea1ab25ea84b14f6328a84622451ceebacace8e
Andriy Kashcha <anvaka@gmail.com>
2019-09-14 10:53:54 -0700

    updated extraction rules

--- top-history/extractSeries.js
+++ top-history/extractSeries.js
@@ -12,6 +12,7 @@ forEachRecord(inputFileName, subredditSnapshot => {
 
 function processSubreddit(subredditSnapshot) {
   let currentTime = new Date(subredditSnapshot.time);
+  currentTime.setSeconds(0);
   let position = 1;
   subredditSnapshot.posts.forEach((post) => processPost(post, currentTime));
 
@@ -23,16 +24,27 @@ function processSubreddit(subredditSnapshot) {
 
     let postDataPoints = getOrCreatePostDataPoints(post.permalink)
     let band = Math.round(elapsed/fiveMinutes);
+    let prevPoint = postDataPoints[postDataPoints.length - 1];
+    const score = post.score || 0;
+    let velocity = getVelocity(prevPoint, band, score);
+
     postDataPoints.push({
       date: currentTime,
-      score: post.score || 0,
       band,
+      score,
+      velocity,
       position,
       comments: post.num_comments || 0
     });
 
     position += 1;
   }
+}
+
+function getVelocity(prevPoint, currentBand, currentScore) {
+  if (!prevPoint) return 0;
+  let bandChange = currentBand - prevPoint.band;
+  return (currentScore - prevPoint.score) / bandChange;
 }
 
 
@@ -47,15 +59,29 @@ function getOrCreatePostDataPoints(postId) {
 }
 
 function saveSeries() {
-  console.log('post_id,date,band,score,comments,position');
+  console.log('post_id,date,band,score,score_final,velocity,comments,position');
   posts.forEach((points, postId) => printPostPoints(points, postId))
 }
 
 function printPostPoints(points, postId) {
+  let scoreAt24hMark = getScoreAt24HMark(points);
+  if (scoreAt24hMark === undefined) {
+    return; // we couldn't collect this, so can't learn from it.
+  }
+  return;
   console.log(
-    points.map(x => [
-        postId, x.date.toISOString(), x.band, x.score, x.comments, x.position
+    points.map(x => {
+      return [
+        postId, x.date.toISOString(), x.band, x.score, scoreAt24hMark, x.velocity, x.comments, x.position
       ].join(',')
-    ).join('\n')
+    }).join('\n')
   );
+}
+
+function getScoreAt24HMark(points) {
+  for (let i = points.length - 1; i > 0; i--) {
+    let post = points[i];
+    if (post.band === 287) return post.score;
+    if (post.band < 287) return;
+  }
 }
