0a797c22e2e94d652f3d8cc49a3cf2e5699453f9
Andriy Kashcha <anvaka@gmail.com>
2019-10-07 00:19:33 -0700

    post selector

---
+++ top-history/animate/src/components/PostSelector.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='template-selector'>
+    <div class='loading'>Loading top posts</div>
+  </div>
+</template>
+
+<script>
+import fetchPosts from '../lib/fetchPosts';
+
+export default {
+  name: 'PostSelector',
+  props: ['subreddit'],
+  data() {
+    return {
+      loading: true,
+      posts: []
+    }
+  },
+  mounted() {
+    fetchPosts().then(posts => {
+      this.posts = posts;
+      this.$emit('loaded', posts);
+    });
+  }
+}
+</script>
