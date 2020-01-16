1c66975a1368d7660e2f309773ae2c4004802c3c
Andriy Kashcha <anvaka@gmail.com>
2020-01-16 00:08:58 -0800

    mini crawler

---
+++ download-osm-roads/index.js
@@ -0,0 +1,129 @@
+let fs = require('fs');
+let fetch = require('node-fetch')
+let forEachLine = require('for-each-line');
+let toProtobuf = require('./lib/toProtobuf');
+let path = require('path')
+let outFileName = path.join(__dirname, 'data', 'processed.json');
+let outgoing;
+let queue = [];
+let executeOSMQuery = require('./lib/executeOSMQuery')
+var JSONStream = require('JSONStream')
+var es = require('event-stream')
+
+readProcessedFile(outFileName, crawl);
+
+function crawl(seen) {
+  forEachLine(process.argv[2] || 'cities.txt', line => {
+    let cityName = sanitize(line)
+    if (!seen.has(cityName)) queue.push(cityName)
+  }).then(downloadAll);
+}
+
+function downloadAll() {
+  let last = 0;
+  downloadNext();
+
+  function downloadNext() {
+    let city = queue[last];
+    if (!city) {
+      console.warn('All done');
+      return;
+    }
+
+    last += 1;
+
+    let url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(city)}`;
+    console.log('Fetching area for: ' + url);
+
+    fetch(url)
+      .then(x => x.json())
+      .then(x => x.filter(row => row.osm_type === 'relation').map(row => ({
+        key: city,
+        name: row.display_name,
+        type: row.type,
+        areaId: row.osm_id + 36e8
+      })))
+      .then(data => {
+        if (!data.length) {
+          console.warn('Could not find matches for ' + city + '; Skipping');
+          downloadNext();
+          return;
+        }
+
+        downloadOSM(data[0]);
+      }).catch(err => {
+        console.error('Error when fetching ' + url)
+        console.error(err);
+      });
+  }
+
+  function downloadOSM(place) {
+    let query = getQuery(place.areaId);
+    return executeOSMQuery(query)
+      .then(data => saveResults(data.elements, place))
+      .then(downloadNext)
+  }
+
+  function saveResults(elements, place) {
+    let buffer = toProtobuf(elements, place.name, place.areaId);
+    let fileName = path.join(__dirname, 'data', place.areaId + '.pbf');
+    fs.writeFileSync(fileName, buffer);
+    markProcessed(place);
+  }
+
+function getQuery(areaId) {
+    return `[timeout:9000][maxsize:2000000000][out:json];
+area(${areaId});
+(._; )->.area;
+(
+way["highway"](area.area);
+node(w);
+);
+out skel;`;
+}
+}
+
+function sanitize(name) {
+  return name.replace(/\t/g, ',');
+}
+
+function markProcessed(page) {
+  if(!outgoing) {
+    createOutStream();
+  }
+
+  outgoing.write(page);
+}
+
+function createOutStream() {
+  outgoing = JSONStream.stringify(false);
+  var fileStream = fs.createWriteStream(outFileName, {
+    encoding: 'utf8',
+    flags: 'a'
+  });
+  outgoing.pipe(fileStream);
+}
+
+function readProcessedFile(fileName, done) {
+  var seen = new Set();
+  if (!fs.existsSync(fileName)) {
+    done(seen);
+    return;
+  }
+
+  console.log('Parsing processed list...');
+  var jsonStreamParser = JSONStream.parse();
+  fs.createReadStream(fileName)
+    .pipe(jsonStreamParser)
+    .pipe(es.mapSync(markProcessed))
+    .on('end', fileInitialized);
+
+  function markProcessed(place) {
+    seen.add(place.key);
+  }
+
+  function fileInitialized() {
+    console.log('Processed: ' + seen.size);
+    done(seen);
+  }
+}
