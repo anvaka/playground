20a4fc165bbbfd8f23a56f5661b36a8b6e53cc9b
Andriy Kashcha <anvaka@gmail.com>
2020-12-30 15:35:38 -0800

    more playing

--- shared-borders/index.js
+++ shared-borders/index.js
@@ -1,6 +1,8 @@
+import simplify from './simplify.js'
 /**
  * A single corner of a polygon
  */
+const pathSimplificationFactor = 128;
 class PolygonNode {
   constructor(coordinates) {
     this.coordinates = coordinates;
@@ -37,12 +39,13 @@ class PolygonGraph {
 
     if (!this.graph.hasNode(fromKey)) {
       this.graph.addNode(fromKey, new PolygonNode(from));
-      this.polygonIdToNodeIds.get(polygonId).add(fromKey);
     }
+    this.polygonIdToNodeIds.get(polygonId).add(fromKey);
+
     if (!this.graph.hasNode(toKey)) {
       this.graph.addNode(toKey, new PolygonNode(to));
-      this.polygonIdToNodeIds.get(polygonId).add(toKey);
     }
+    this.polygonIdToNodeIds.get(polygonId).add(toKey);
 
     if (fromKey > toKey) {
       let t = fromKey; fromKey = toKey; toKey = t;
@@ -58,20 +61,119 @@ class PolygonGraph {
     }
   }
 
+  removeSegment(fromKey, toKey) {
+    if (fromKey > toKey) {
+      let t = fromKey; fromKey = toKey; toKey = t;
+    }
+    let link = this.graph.getLink(fromKey, toKey);
+    if (link) {
+      this.graph.removeLink(link);
+      this._removeZeroDegreeNode(fromKey);
+      this._removeZeroDegreeNode(toKey);
+    } else {
+      console.error('trying to remove non-existing segment');
+    }
+  }
+  _removeZeroDegreeNode(nodeId) {
+    let node = this.graph.getNode(nodeId);
+    if (node.links.length > 0) return;
+    node.data.connections.forEach(polygonId => {
+      this.polygonIdToNodeIds.get(polygonId).delete(nodeId);
+    });
+    this.graph.removeNode(nodeId);
+  }
+
+  simplifyPolygonBorders(polygonId) {
+    const polygon = map.getPolygon(polygonId);
+    if (!polygon) return;
+    let segment = [];
+    let lastProcessedNode = 0;
+    while(lastProcessedNode < polygon.length) {
+      segment.push(polygon[lastProcessedNode]);
+
+      if (polygon[lastProcessedNode].degree === 3) {
+        this.makeInternalSegmentSimple(polygonId, segment);
+        segment = [polygon[lastProcessedNode]];
+      }
+      lastProcessedNode += 1;
+    }
+    this.makeInternalSegmentSimple(polygonId, segment);
+  }
+
+  makeInternalSegmentSimple(polygonId, segment) {
+    // let newPath = orthogonalizePath(segment, 1800);
+    let newPath = simplifyPath(segment, pathSimplificationFactor);
+    let firstTwoDegree = segment.find(x => x.degree === 2);
+    let polygons = new Set([polygonId]);
+    if (firstTwoDegree) {
+      this.graph.getNode(firstTwoDegree.id).data.connections.forEach(connectionId => {
+        polygons.add(connectionId)
+      });
+    }
+    let polygonIds = Array.from(polygons);
+
+    for (let i = 1; i < segment.length; ++i) {
+      this.removeSegment(segment[i - 1].id, segment[i].id);
+    }
+    for (let i = 1; i < newPath.length; ++i) {
+      polygonIds.forEach(polygonId => {
+        let shouldUpdateSegment = false;
+        if (this.graph.hasNode(newPath[i - 1].id)) {
+          shouldUpdateSegment = true;
+          this.graph.getNode(newPath[i - 1].id).data.coordinates = newPath[i - 1].point;
+        }
+        if (this.graph.hasNode(newPath[i].id)) {
+          shouldUpdateSegment = true;
+          this.graph.getNode(newPath[i].id).data.coordinates = newPath[i].point;
+        } 
+        
+        let from = newPath[i - 1].point;
+        let to = newPath[i].point;
+        if (shouldUpdateSegment) {
+          let fromKey = newPath[i - 1].id || this.getSegmentKey(newPath[i - 1].point)
+          let toKey = newPath[i].id || this.getSegmentKey(newPath[i].point);
+          if (fromKey > toKey) {
+            let t = fromKey; fromKey = toKey; toKey = t;
+            t = from; from = to; to = t;
+          }
+
+
+          if (!this.graph.hasNode(fromKey)) {
+            this.graph.addNode(fromKey, new PolygonNode(from));
+          }
+          this.graph.getNode(fromKey).data.connect(polygonId);
+
+          if (!this.graph.hasNode(toKey)) {
+            this.graph.addNode(toKey, new PolygonNode(to));
+          }
+          this.graph.getNode(toKey).data.connect(polygonId);
+
+          if (fromKey === toKey) return;
+
+          if (!this.graph.hasLink(fromKey, toKey)) {
+            this.graph.addLink(fromKey, toKey, new PolygonEdge(fromKey, toKey))
+          }
+        } else {
+          this.addSegment(polygonId, from, to);
+        }
+      })
+    }
+  }
+
   getSegmentKey(segment) {
     return `${segment[0]},${segment[1]}`
   }
 
   getAllPolygons() {
     let polygons = [];
+    this.polygonIdToNodeIds.forEach((points, polygonId) => polygons.push(this.getPolygon(polygonId)));
+    return polygons.filter(x => x);
+  }
 
-    this.polygonIdToNodeIds.forEach((points, polygonId) => {
-      let startFrom = Array.from(points)[0];
-      let polygon = this.getPolygonStartingFrom(startFrom, polygonId);
-      polygons.push(polygon);
-    });
-
-    return polygons;
+  getPolygon(polygonId) {
+    let points = this.polygonIdToNodeIds.get(polygonId);
+    let startFrom = Array.from(points)[0];
+    return this.getPolygonStartingFrom(startFrom, polygonId);
   }
 
   getPolygonStartingFrom(startFromNodeId, polygonId) {
@@ -86,6 +188,7 @@ class PolygonGraph {
       if (!polygonNode.links) throw new Error('Each node should be connected');
 
       polygon.push({
+        id: nodeId,
         point: polygonNode.data.coordinates,
         degree: polygonNode.links.length
       });
@@ -105,30 +208,154 @@ class PolygonGraph {
   }
 }
 
-const map = new PolygonGraph();
-Array.from(document.querySelectorAll('path')).forEach((path, pathId) => {
-  let points = path.getAttributeNS(null, 'd')
-    .replace(/[MLZ]/i, '')
-    .split(' ')
-    .map(pair => pair.split(',').map(x => Number.parseFloat(x)))
+function simplifyPath(path, minLength) {
+  let res = simplify(path, minLength);
+  let points = res.map(x => ({
+    point: [x.point[0], x.point[1]],
+    degree: x.degree,
+    id: x.id
+  }));
+
+  let angleThreshold = Math.PI * 15 / 180;
 
   for (let i = 1; i < points.length; ++i) {
-    map.addSegment(pathId, points[i - 1], points[i]);
-  }
-});
+    let prev = points[i - 1];
+    let current = points[i];
+    let dx = current.point[0] - prev.point[0];
+    let dy = current.point[1] - prev.point[1];
+    let angle = Math.abs(Math.atan2(dy, dx));
+    // if (Math.PI / 4 - angle  > 0 && Math.PI / 4 - angle < angleThreshold) {
+    //   console.log('r')
+    //   let rotated = rotateAroundPoint(prev.point, current.point,  Math.PI / 4 - angle)
+    //   current.point[0] = rotated[0]
+    //   current.point[1] = rotated[1]
+    // }
 
-let appendTo = document.querySelector('#result');
-let polygons = map.getAllPolygons();
-polygons.forEach(polygon => {
-  let path = window.sivg('path', {
-    d: getPolygonData(polygon),
-    stroke: 'black',
-    'stroke-width': 10,
-    fill: 'transparent'
-  })
-  appendTo.appendChild(path)
-});
+    if (angle < angleThreshold || (Math.PI - angle < angleThreshold) ) {
+      // console.log(`Angle between (${prev[0]},${prev[1]}) and (${current[0]},${current[1]}) is ${angle}. Correcting`)
+      // equalize y coordinate;
+      current.point[1] = prev.point[1];
+      // else if (prev.degree < 3) prev.point[1] = current.point[1];
+    } else if (Math.abs(Math.PI / 2 - angle) < angleThreshold) {
+      current.point[0] = prev.point[0];
+    }
+  }
+  return points;
+}
+
+function rotateAroundPoint(pivotPoint, targetPoint, angle) {
+  let [cx, cy] = pivotPoint;
+  let [tx, ty] = targetPoint;
+  let x = tx - cx;
+  let y = ty - cy;
+  let xNew = x * Math.cos(angle) - y * Math.sin(angle);
+  let yNew = x * Math.sin(angle) + y * Math.cos(angle);
+  return [cx + xNew, cy + yNew];
+}
+
+function orthogonalizePath(path, minLength) {
+  let breakPoints = [];
+  let l = 0;
+  for (let i = 1; i < path.length- 1; ++i) {
+    let prev = path[i - 1];
+    let current = path[i];
+    let dist = getSegmentLength(prev, current);
+    if (dist + l >= minLength) {
+      breakPoints.push(i);
+      l = 0;
+    } else {
+      l += dist;
+    }
+  }
+  
+  let result = [path[0].point];
+  breakPoints.forEach(pointIndex => {
+    let target = path[pointIndex].point;
+    let prev = result[result.length - 1];
+    result.push([prev[0], target[1]])
+    result.push(target);
+  });
+  result.push(path[path.length - 1].point);
+  return result;
+}
+
+function getSegmentLength(a, b) {
+  let dx = a.point[0] - b.point[0];
+  let dy = a.point[1] - b.point[1];
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+const map = new PolygonGraph();
+
+
+function drawResults() {
+  let appendTo = document.querySelector('#result');
+  let polygons = map.getAllPolygons();
+
+  map.polygonIdToNodeIds.forEach((nodes, polygonId) => {
+      map.simplifyPolygonBorders(polygonId);
+  });
+
+  polygons = map.getAllPolygons();
+  polygons.forEach(polygon => {
+    let path = window.sivg('path', {
+      d: getPolygonData(polygon),
+      stroke: 'red',
+      'stroke-width': 40,
+      fill: 'transparent'
+    })
+    appendTo.appendChild(path)
+    polygon.forEach(({point}) => {
+      // let txt = window.sivg('text', {
+      //   x: point[0],
+      //   y: point[1],
+      //   'fill': 'orange',
+      //   'font-size': 42
+      // });
+      // txt.text(point.join(','));
+      // appendTo.appendChild(txt)
+    })
+  });
+}
+
+function loadFromSVG() {
+  Array.from(document.querySelectorAll('path')).forEach((path, pathId) => {
+    let points = path.getAttributeNS(null, 'd')
+      .replace(/[MLZ]/i, '')
+      .split(' ')
+      .map(pair => pair.split(',').map(x => Number.parseFloat(x)))
+
+    for (let i = 1; i < points.length; ++i) {
+      map.addSegment(pathId, points[i - 1], points[i]);
+    }
+  });
+}
 
 function getPolygonData(polygon) {
   return polygon.map(({point}, index) => index === 0 ? 'M' + point.join(',') + 'L': point.join(',')).join(' ');
 }
+
+loadFromSVG();
+// map.addSegment(0, [0, 0], [0, 600]);
+// map.addSegment(0, [600, 600], [0, 600]);
+// map.addSegment(0, [600, 600], [600, -100]);
+// map.addSegment(0, [0, 0], [600, -100]);
+
+// map.addSegment(1, [600, 600], [600, -100]);
+// map.addSegment(1, [600, 600], [800, 800]);
+// map.addSegment(1, [800, -600], [800, 800]);
+// map.addSegment(1, [800, -600], [600, -100]);
+drawResults();
+// fetch('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson', {
+//   mode: 'cors'
+// }).then(x => x.json()).then(x => {
+//   console.log(x);
+//   x.features.forEach((feature, polygonId) => {
+//     feature.geometry.coordinates[0].forEach((pair, index, arr) => {
+//       if (index > 0) {
+//         map.addSegment(polygonId, arr[index - 1], arr[index])
+//       }
+//     })
+//   });
+//   drawResults();
+// })
---
+++ shared-borders/simplify.js
@@ -0,0 +1,85 @@
+/**
+Copyright (c) 2012, Vladimir Agafonkin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+function getSqSegDist(p, p1, p2) {
+    var x = p1[0],
+        y = p1[1],
+        dx = p2[0] - x,
+        dy = p2[1] - y;
+
+    if (dx !== 0 || dy !== 0) {
+
+        var t = ((p[0] - x) * dx + (p[1] - y) * dy) / (dx * dx + dy * dy);
+
+        if (t > 1) {
+            x = p2[0];
+            y = p2[1];
+
+        } else if (t > 0) {
+            x += dx * t;
+            y += dy * t;
+        }
+    }
+
+    dx = p[0] - x;
+    dy = p[1] - y;
+
+    return dx * dx + dy * dy;
+}
+
+function simplifyDPStep(points, first, last, sqTolerance, simplified) {
+    var maxSqDist = sqTolerance,
+        index;
+
+    for (var i = first + 1; i < last; i++) {
+        var sqDist = getSqSegDist(points[i].point, points[first].point, points[last].point);
+
+        if (sqDist > maxSqDist) {
+            index = i;
+            maxSqDist = sqDist;
+        }
+    }
+
+    if (maxSqDist > sqTolerance) {
+        if (index - first > 1) simplifyDPStep(points, first, index, sqTolerance, simplified);
+        simplified.push(points[index]);
+        if (last - index > 1) simplifyDPStep(points, index, last, sqTolerance, simplified);
+    }
+}
+
+// simplification using Ramer-Douglas-Peucker algorithm
+export default function simplifyDouglasPeucker(points, tolerance) {
+    if (points.length<=1)
+        return points;
+    tolerance = typeof tolerance === 'number' ? tolerance : 1;
+    var sqTolerance = tolerance * tolerance;
+    
+    var last = points.length - 1;
+
+    var simplified = [points[0]];
+    simplifyDPStep(points, 0, last, sqTolerance, simplified);
+    simplified.push(points[last]);
+
+    return simplified;
+}
