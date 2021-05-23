733e3d80bf818cf66328733abdaf2da55a5b6de1
anvaka <anvaka@gmail.com>
2021-05-23 11:51:02 -0700

    updated path parser

--- path-parsers/index.js
+++ path-parsers/index.js
@@ -12,7 +12,7 @@ let counters = {
   it: 0
 }
 
-const characterLookup = {
+const CharacterLookup = {
   '0': 0,
   '1': 1,
   '2': 2,
@@ -25,29 +25,213 @@ const characterLookup = {
   '9': 9
 }
 
+/**
+ * Naive parser of integer numbers. Optimized for memory consumption and
+ * CPU performance. Not very strong on validation side.
+ */
 class NumberParser {
   constructor() {
     this.value = 0;
-    this.hasValue = false;
+    this.fractionValue = 0;
+    this.divider = 1;
+    this.exponent = 0;
     this.isNegative = false;
+    this.hasValue = false;
+    this.hasFraction = false;
+    this.hasExponent = false
   }
   getValue() {
-    return this.isNegative ? -this.value : this.value;
+    let value = this.value;
+    if (this.hasFraction) {
+      value += this.fractionValue / this.divider;
+    }
+    if (this.hasExponent) {
+      value *= Math.pow(10, this.exponent);
+    }
+    if (this.isNegative) {
+      return -value;
+    }
+    return value;
   }
   reset() {
-    this.hasValue = false;
     this.value = 0;
+    this.fractionValue = 0;
+    this.divider = 1;
+    this.exponent = 0;
     this.isNegative = false;
+    this.hasValue = false;
+    this.hasFraction = false;
+    this.hasExponent = false
   }
   addCharacter(ch) {
     this.hasValue = true;
-    if (ch === '-') this.isNegative = true;
+    if (ch === '-') {
+      this.isNegative = true;
+      return;
+    }
+    if (ch === '.') {
+      if (this.hasFraction) throw new Error('Already has fractional part!');
+      this.hasFraction = true;
+      return;
+    }
+    if (ch === 'e') {
+      if (this.hasExponent) throw new Error('Already has exponent');
+      this.hasExponent = true;
+      this.exponent = 0;
+      return;
+    }
 
-    let numValue = characterLookup[ch];
+    let numValue = CharacterLookup[ch];
     if (numValue === undefined) throw new Error('Not a digit: ' + ch)
 
-    this.value = this.value * 10 + numValue;
+    if (this.hasExponent) {
+      this.exponent = this.exponent * 10 + numValue;
+    } else if (this.hasFraction) {
+      this.fractionValue = this.fractionValue * 10 + numValue;
+      this.divider *= 10;
+    } else {
+      this.value = this.value * 10 + numValue;
+    }
   }
+}
+
+const processCommand = {
+  M(points, lastNumbers) {
+    // TODO: validate lastNumbers.length % 2 === 0
+    if (points.length === 0) {
+      // consider this to be absolute points
+      for (let i = 0; i < lastNumbers.length; i += 2) {
+        points.push([lastNumbers[i], lastNumbers[i + 1]]);
+      }
+    } else {
+      // Note: this is not true for generic case, and could/should be extended to start a new path.
+      // We are just optimizing for own sake of a single path
+      throw new Error('Only one "Move" command per path is expected');
+    }
+  },
+  m(points, lastNumbers) {
+    let lx = 0, ly = 0;
+    if (points.length > 0) {
+      let last = points[points.length - 1];
+      lx = last[0];
+      ly = last[1];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 2) {
+      let x = lx + lastNumbers[i];
+      let y = ly + lastNumbers[i + 1];
+      points.push([x, y]);
+      lx = x; ly = y;
+    }
+  },
+  // line to:
+  L(points, lastNumbers) {
+    // TODO: validate lastNumbers.length % 2 === 0
+    for (let i = 0; i < lastNumbers.length; i += 2) {
+      points.push([lastNumbers[i], lastNumbers[i + 1]]);
+    }
+  },
+  // relative line to:
+  l(points, lastNumbers) {
+    let lx = 0, ly = 0;
+    if (points.length > 0) {
+      let last = points[points.length - 1];
+      lx = last[0];
+      ly = last[1];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 2) {
+      let x = lx + lastNumbers[i];
+      let y = ly + lastNumbers[i + 1];
+      points.push([x, y]);
+      lx = x; ly = y;
+    }
+  },
+  H(points, lastNumbers) {
+    let y = 0;
+    if (points.length > 0) {
+      y = points[points.length - 1][1];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 1) {
+      let x = lastNumbers[i];
+      points.push([x, y]);
+    }
+  },
+  h(points, lastNumbers) {
+    let y = 0, lx = 0;
+    if (points.length > 0) {
+      lx = points[points.length - 1][0];
+      y = points[points.length - 1][1];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 1) {
+      let x = lx + lastNumbers[i];
+      points.push([x, y]);
+      lx = x;
+    }
+  },
+  V(points, lastNumbers) {
+    let x = 0;
+    if (points.length > 0) {
+      x = points[points.length - 1][0];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 1) {
+      points.push([x, lastNumbers[i]]);
+    }
+  },
+  v(points, lastNumbers) {
+    let ly = 0, x = 0;
+    if (points.length > 0) {
+      x = points[points.length - 1][0];
+      ly = points[points.length - 1][1];
+    }
+    for (let i = 0; i < lastNumbers.length; i += 1) {
+      let y = ly + lastNumbers[i];
+      points.push([x, y]);
+      ly = y;
+    }
+  }
+}
+
+function getPointsFromPathData(d) {
+  let numParser = new NumberParser();
+  let idx = 0;
+  let l = d.length;
+  let ch;
+  let lastNumbers, lastCommand;
+  let points = [];
+  while (idx < l) {
+    ch = d[idx];
+    if (ch in processCommand) {
+      if (numParser.hasValue) {
+        lastNumbers.push(numParser.getValue())
+      }
+      numParser.reset();
+      if (lastNumbers) lastCommand(points, lastNumbers);
+      lastCommand = processCommand[ch];
+      lastNumbers = [];
+    } else if (ch === ' ' || ch === ',') {
+      if (numParser.hasValue) {
+        lastNumbers.push(numParser.getValue())
+        numParser.reset();
+      }
+      // ignore.
+    } else if (ch === 'Z' || ch === 'z') {
+      // ignore
+    } else if (numParser.hasValue && ch === '-') {
+      // this considered to be a start of the next number.
+      lastNumbers.push(numParser.getValue())
+      numParser.reset();
+      numParser.addCharacter(ch);
+    } else {
+      numParser.addCharacter(ch);
+    }
+    idx += 1;
+  }
+  if (numParser.hasValue) {
+    lastNumbers.push(numParser.getValue());
+  }
+  if (lastNumbers) {
+    lastCommand(points, lastNumbers);
+  }
+  return points;
 }
  
 // debugger;
@@ -63,7 +247,7 @@ suite.add('svg parser', function() {
   counters.my += myParser(d)
 })
 .add('my fast parser', function() {
-  counters.myFast += myFastParser(d).length;
+  counters.myFast += getPointsFromPathData(d).length;
 })
 .add('iterator', function() {
   counters.it += iterator(d)
@@ -96,48 +280,4 @@ function iterator(d) {
     if (ch === '0') count += 1;
   }
   return count;
-}
-
-
-
-function myFastParser(d) {
-  // let buff;
-  let numParser = new NumberParser();
-  let idx = 0;
-  let l = d.length;
-  let ch;
-  let ctx;
-  let lastNumbers;
-  let points = [];
-  while (idx < l) {
-    ch = d[idx];
-    if (ch === 'M' || ch === 'L') {
-      if (numParser.hasValue) {
-        lastNumbers.push(numParser.getValue())
-      }
-      numParser.reset();
-      lastCommand = ch;
-      if (lastNumbers) {
-        lastNumbers.forEach(num => {points.push(num);});
-      }
-      lastNumbers = [];
-    } else if (ch === ' ' || ch === ',') {
-      if (numParser.hasValue) {
-        lastNumbers.push(numParser.getValue())
-        numParser.reset();
-      }
-      // ignore.
-    } else if (ch === 'Z') {
-      // ignore
-    } else {
-      numParser.addCharacter(ch);
-      // buff.push(ch)
-    }
-    idx += 1;
-  }
-  if (numParser.hasValue) {
-    lastNumbers.push(numParser.getValue());
-    lastNumbers.forEach(num => {points.push(num);});
-  }
-  return points;
 }
