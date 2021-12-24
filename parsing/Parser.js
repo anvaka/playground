ddd7b32762ff6a3e523eac2cb5a7464515fd2546
anvaka <anvaka@gmail.com>
2021-12-24 10:23:53 -0800

    Simplify

--- parsing/Parser.js
+++ parsing/Parser.js
@@ -4,25 +4,26 @@ export class Parser {
   constructor(tokens) {
     this.prefixParselets = new Map();
     this.infixParselets = new Map();
+    this.precedenceMap = new Map();
 
     this.tokens = tokens;
     this.current = 0;
   }
 
-  parseExpression(precedence) {
-    if (precedence === undefined) precedence = 0;
+  parseExpression(parselet) {
+    let precedence = this.precedenceMap.get(parselet) || 0;
     let token = this.consume();
     let prefixParselet = this.prefixParselets.get(token.kind);
     if (!prefixParselet) {
       throw new Error(`No prefix parselet for ${token.kind}`);
     }
     
-    let left = prefixParselet.parse(this, token);
+    let left = prefixParselet(this, token);
 
     while (precedence < this.getPrecedence() && this.peek()) {
       token = this.consume();
-      let infix = this.infixParselets.get(token.kind);
-      left = infix.parse(this, left, token);
+      let infixParselet = this.infixParselets.get(token.kind);
+      left = infixParselet(this, token, left);
     }
 
     return left;
@@ -31,8 +32,7 @@ export class Parser {
   getPrecedence() {
     let token = this.peek();
     if (!token) return 0;
-    let infix = this.infixParselets.get(token.kind);
-    return infix ? infix.getPrecedence() : 0;
+    return this.precedenceMap.get(this.infixParselets.get(token.kind)) || 0;
   }
 
   consume(expectedKind) {
@@ -48,8 +48,7 @@ export class Parser {
   }
 
   peek() {
-    if (this.current >= this.tokens.length) return null;
-    return this.tokens[this.current];
+    return this.tokens[this.current] || null;
   }
 
   registerPrefixParselet(tokenKind, prefixParselet) {
@@ -60,85 +59,51 @@ export class Parser {
     this.prefixParselets.set(tokenKind, prefixParselet);
   }
 
-  registerInfixParselet(tokenKind, infixParselet) {
+  registerInfixParselet(tokenKind, infixParselet, priority) {
     if (this.infixParselets.has(tokenKind)) {
       throw new Error(`Infix parselet already registered for token kind ${tokenKind}`);
     }
     this.infixParselets.set(tokenKind, infixParselet);
+    this.precedenceMap.set(infixParselet, priority || 0);
   }
 }
 
-export class Expression {}
-
-export class NameExpression extends Expression {
-  constructor(value) {
-    super()
-    this.value = value;
-  }
+export class NameExpression {
+  constructor(value) { this.value = value; }
 }
 
-export class BinaryExpression extends Expression {
+export class BinaryExpression {
   constructor(left, operator, right) {
-    super();
     this.left = left;
     this.operator = operator;
     this.right = right;
   }
 }
 
-export class PrefixExpression extends Expression {
+export class PrefixExpression {
   constructor(operator, right) {
-    super();
     this.operator = operator;
     this.right = right;
   }
 }
 
 // Prefix parsing
-export class PrefixParselet {
-  parse(parser, token) { throw new Error('Not implemented'); }
+export function nameParselet(parser, token) { 
+  return new NameExpression(token.value);
 }
 
-export class NameParselet extends PrefixParselet {
-  constructor() { super(); }
-
-  parse(parser, token) {
-    return new NameExpression(token.value);
-  }
+export function parenthesisParselet(parser, token) {
+  let expression = parser.parseExpression();
+  parser.consume(TokenKind.RPAREN);
+  return expression;
 }
 
-export class ParenthesisParselet extends PrefixParselet {
-  constructor() { super(); }
-
-  parse(parser, token) {
-    let expression = parser.parseExpression();
-    parser.consume(TokenKind.RPAREN);
-    return expression;
-  }
-}
-
-export class PrefixOperatorParselet extends PrefixParselet {
-  parse(parser, token) {
-    let operand = parser.parseExpression();
-    return new PrefixExpression(token.value, operand);
-  }
+export function prefixParselet(parser, token) {
+  let operand = parser.parseExpression();
+  return new PrefixExpression(token.value, operand);
 }
 
 // Infix parsing
-export class InfixParselet {
-  parse(parser, left, token) { throw new Error('Not implemented'); }
-  getPrecedence() { throw new Error('Not implemented'); }
-}
-
-export class BinaryOperatorParselet  extends InfixParselet {
-  constructor(precedence) { 
-    super(); 
-    this.precedence = precedence;
-  }
-
-  parse(parser, left, token) {
-    return new BinaryExpression(left, token.value, parser.parseExpression(this.getPrecedence()));
-  }
-
-  getPrecedence() { return this.precedence; }
+export function binaryOperatorParselet(parser, token, left) {
+  return new BinaryExpression(left, token.value, parser.parseExpression(this));
 }
--- parsing/test.js
+++ parsing/test.js
@@ -1,4 +1,4 @@
-import {BinaryOperatorParselet, NameParselet, Parser, PrefixOperatorParselet, ParenthesisParselet} from './Parser.js'
+import {binaryOperatorParselet, nameParselet, Parser, prefixParselet, parenthesisParselet} from './Parser.js'
 import {Lexer, TokenKind} from './Lexer.js'
 
 // Working through http://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
@@ -9,19 +9,18 @@ console.log(tokens);
 
 const parser = new Parser(tokens);
 
-const prefixParselet = new PrefixOperatorParselet();
 parser.registerPrefixParselet(TokenKind.PLUS, prefixParselet);
 parser.registerPrefixParselet(TokenKind.MINUS, prefixParselet);
 parser.registerPrefixParselet(TokenKind.MULTIPLY, prefixParselet);
 parser.registerPrefixParselet(TokenKind.DIVIDE, prefixParselet);
-parser.registerPrefixParselet(TokenKind.LPAREN, new ParenthesisParselet());
-parser.registerPrefixParselet(TokenKind.IDENTIFIER, new NameParselet());
-parser.registerPrefixParselet(TokenKind.NUMBER, new NameParselet());
+parser.registerPrefixParselet(TokenKind.LPAREN, parenthesisParselet);
+parser.registerPrefixParselet(TokenKind.IDENTIFIER, nameParselet);
+parser.registerPrefixParselet(TokenKind.NUMBER, nameParselet);
 
-parser.registerInfixParselet(TokenKind.MINUS, new BinaryOperatorParselet(1))
-parser.registerInfixParselet(TokenKind.PLUS, new BinaryOperatorParselet(2))
-parser.registerInfixParselet(TokenKind.MULTIPLY, new BinaryOperatorParselet(3))
-parser.registerInfixParselet(TokenKind.DIVIDE, new BinaryOperatorParselet(4))
+parser.registerInfixParselet(TokenKind.MINUS, binaryOperatorParselet, 1);
+parser.registerInfixParselet(TokenKind.PLUS, binaryOperatorParselet, 2);
+parser.registerInfixParselet(TokenKind.MULTIPLY, binaryOperatorParselet, 3);
+parser.registerInfixParselet(TokenKind.DIVIDE, binaryOperatorParselet, 4);
 
 let result = parser.parseExpression()
 console.log(result)
