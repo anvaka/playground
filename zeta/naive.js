1cb5e3484c28bb4a010e3608c94a3ed4497d6730
anvaka <anvaka@gmail.com>
2022-10-25 13:50:31 -0400

    playing with zeta function

---
+++ zeta/naive.js
@@ -0,0 +1,19 @@
+// Naive computation of zeta function
+
+export function zetaReal(s, iterations = 1000) {
+  let sum = 0;
+  for (let n = 1; n < iterations; n++) {
+    sum += 1 / Math.pow(n, s);
+  }
+  return sum;
+}
+
+export function zetaComplex(s, iterations = 1000) {
+  let sum = { re: 0, im: 0 };
+  // c ^ (a + b i) = c ^ a (cos(b ln(c)) + i sin(b ln(c)))
+  for (let n = 1; n < iterations; n++) {
+    sum.re += Math.pow(n, -s.re) * Math.cos(s.im * Math.log(n));
+    sum.im += Math.pow(n, -s.re) * Math.sin(s.im * Math.log(n));
+  }
+  return sum;
+}
